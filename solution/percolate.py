7e80ab6a4bb825f7e44dc05d00af1f1b3871e9b5
Zack Zlotnik <zackzlotnik@gmail.com>
2018-05-19 10:07:47 -0400

    autopep8 run

--- solution/percolate.py
+++ solution/percolate.py
@@ -4,178 +4,190 @@ import json
 
 
 class FormatParser(object):
-  """Parent class for all formats to be parsed."""
+    """Parent class for all formats to be parsed."""
 
-  def __init__(self, entry):
-    """Constructor.
+    def __init__(self, entry):
+        """Constructor.
 
-    Args:
-      data: string; The line from the given input file to parse.
-    """
-    self.entry = [item.strip() for item in entry.split(',')]
+        Args:
+          data: string; The line from the given input file to parse.
+        """
+        self.entry = [item.strip() for item in entry.split(',')]
 
-  def GetFormattedData(self):
-    """Formats and returns the provided data, if valid.
+    def GetFormattedData(self):
+        """Formats and returns the provided data, if valid.
 
-    Returns:
-      A dictionary of the data given if valid, False otherwise.
-    """
-    parsed_data = self.ParseFormat()
-    if not parsed_data:
-      return False
+        Returns:
+          A dictionary of the data given if valid, False otherwise.
+        """
+        parsed_data = self.ParseFormat()
+        if not parsed_data:
+            return False
 
-    phonenumber = self.FormatPhoneNumber(parsed_data['phonenumber'])
-    if not phonenumber:
-      return False
-    parsed_data['phonenumber'] = phonenumber
- 
-    if not self.IsValidZipcode(parsed_data['zipcode']):
-      return False
+        phonenumber = self.FormatPhoneNumber(parsed_data['phonenumber'])
+        if not phonenumber:
+            return False
+        parsed_data['phonenumber'] = phonenumber
 
-    return parsed_data
+        if not self.IsValidZipcode(parsed_data['zipcode']):
+            return False
 
-  def IsValidZipcode(self, zipcode):
-    """Determines if a zipcode is valid.
+        return parsed_data
 
-    Args:
-      zipcode: string; The zipcode to validate.
+    def IsValidZipcode(self, zipcode):
+        """Determines if a zipcode is valid.
 
-    Returns:
-      True, if the zipcode is valid.
-    """
-    stripped_zipcode = strip_nonnumeric_characters(zipcode)
-    return len(stripped_zipcode) == 5
+        Args:
+          zipcode: string; The zipcode to validate.
 
-  def FormatPhoneNumber(self, phonenumber):
-    """Normalizes a phone number into a given format.
+        Returns:
+          True, if the zipcode is valid.
+        """
+        stripped_zipcode = strip_nonnumeric_characters(zipcode)
+        return len(stripped_zipcode) == 5
 
-    Args:
-      phonenumber: string; The phone number to format.
+    def FormatPhoneNumber(self, phonenumber):
+        """Normalizes a phone number into a given format.
 
-    Returns:
-      The formatted phone number.
-    """
-    stripped_phone_number = strip_nonnumeric_characters(phonenumber)
+        Args:
+          phonenumber: string; The phone number to format.
 
-    if len(stripped_phone_number) == 10:
-      return '%s-%s-%s' % (stripped_phone_number[0:3],
-                           stripped_phone_number[3:6],
-                           stripped_phone_number[6:10])
+        Returns:
+          The formatted phone number.
+        """
+        stripped_phone_number = strip_nonnumeric_characters(phonenumber)
+
+        if len(stripped_phone_number) == 10:
+            return '%s-%s-%s' % (stripped_phone_number[0:3],
+                                 stripped_phone_number[3:6],
+                                 stripped_phone_number[6:10])
+
 
 class FormatParser1(FormatParser):
-  """Parser for this format: Lastname, Firstname, (703)-742-0996, Blue, 10013
-  """
-  def ParseFormat(self):
-    """Parses entry data.
-
-    Returns:
-      A dictionary of parsed data.
+    """Parser for this format: Lastname, Firstname, (703)-742-0996, Blue, 10013
     """
-    if len(self.entry) != 5:
-      return False
 
-    lastname, firstname, phonenumber, color, zipcode = self.entry
-    return dict(lastname=lastname,
-                firstname=firstname,
-                phonenumber=phonenumber,
-                color=color,
-                zipcode=zipcode)
+    def ParseFormat(self):
+        """Parses entry data.
+
+        Returns:
+          A dictionary of parsed data.
+        """
+        if len(self.entry) != 5:
+            return False
+
+        lastname, firstname, phonenumber, color, zipcode = self.entry
+        return dict(lastname=lastname,
+                    firstname=firstname,
+                    phonenumber=phonenumber,
+                    color=color,
+                    zipcode=zipcode)
+
 
 class FormatParser2(FormatParser):
-  """Parser for this format: Firstname Lastname, Red, 11237, 703 955 0373
-  """
-  def ParseFormat(self):
-    """Parses entry data.
-
-    Returns:
-      A dictionary of parsed data.
+    """Parser for this format: Firstname Lastname, Red, 11237, 703 955 0373
     """
-    if len(self.entry) != 4:
-      return False
 
-    fullname, color, zipcode, phonenumber = self.entry
-    splitname = fullname.split(' ')
-    firstname = ' '.join(splitname[0:-1])
-    lastname = splitname[-1]
+    def ParseFormat(self):
+        """Parses entry data.
 
-    return dict(lastname=lastname,
-                firstname=firstname,
-                phonenumber=phonenumber,
-                color=color,
-                zipcode=zipcode)
+        Returns:
+          A dictionary of parsed data.
+        """
+        if len(self.entry) != 4:
+            return False
+
+        fullname, color, zipcode, phonenumber = self.entry
+        splitname = fullname.split(' ')
+        firstname = ' '.join(splitname[0:-1])
+        lastname = splitname[-1]
+
+        return dict(lastname=lastname,
+                    firstname=firstname,
+                    phonenumber=phonenumber,
+                    color=color,
+                    zipcode=zipcode)
+
 
 class FormatParser3(FormatParser):
-  """Parser for this format: Firstname, Lastname, 10013, 646 111 0101, Green
-  """
-  def ParseFormat(self):
-    """Parses entry data.
-
-    Returns:
-      A dictionary of parsed data.
+    """Parser for this format: Firstname, Lastname, 10013, 646 111 0101, Green
     """
-    if len(self.entry) != 5:
-      return False
 
-    firstname, lastname, zipcode, phonenumber, color = self.entry
+    def ParseFormat(self):
+        """Parses entry data.
 
-    return dict(lastname=lastname,
-                firstname=firstname,
-                phonenumber=phonenumber,
-                color=color,
-                zipcode=zipcode)
+        Returns:
+          A dictionary of parsed data.
+        """
+        if len(self.entry) != 5:
+            return False
+
+        firstname, lastname, zipcode, phonenumber, color = self.entry
+
+        return dict(lastname=lastname,
+                    firstname=firstname,
+                    phonenumber=phonenumber,
+                    color=color,
+                    zipcode=zipcode)
+
 
 def strip_nonnumeric_characters(string_to_strip):
-  return ''.join((character
-                  for character in string_to_strip
-                  if character.isdigit()))
+    return ''.join((character
+                    for character in string_to_strip
+                    if character.isdigit()))
+
 
 def get_parsed_entry(entry_to_parse):
-  """Runs an entry through each of the parsers to determine a match.
+    """Runs an entry through each of the parsers to determine a match.
 
-  Args:
-    data_to_parse: string; The data to parse from the file.
+    Args:
+      data_to_parse: string; The data to parse from the file.
 
-  Returns:
-    A dictionary of parsed data, if it matches one of the formats. False if not.
-  """
-  return (FormatParser1(entry_to_parse).GetFormattedData() or
-          FormatParser2(entry_to_parse).GetFormattedData() or
-          FormatParser3(entry_to_parse).GetFormattedData() or
-          False)
+    Returns:
+      A dictionary of parsed data, if it matches one of the formats. False if not.
+    """
+    return (FormatParser1(entry_to_parse).GetFormattedData() or
+            FormatParser2(entry_to_parse).GetFormattedData() or
+            FormatParser3(entry_to_parse).GetFormattedData() or
+            False)
+
 
 def parse_data(data_to_parse):
-  """Runs data through the various parsers to determine a match.
+    """Runs data through the various parsers to determine a match.
 
-  Args:
-    data_to_parse: list; A list of the data to parse.
+    Args:
+      data_to_parse: list; A list of the data to parse.
 
-  Returns:
-    A dictionary containing the parsed entries and what lines of the file
-    an error occurred on.
-  """
-  raw_entries = ((get_parsed_entry(entry), i)
-                 for i, entry in enumerate(data_to_parse))
+    Returns:
+      A dictionary containing the parsed entries and what lines of the file
+      an error occurred on.
+    """
+    raw_entries = ((get_parsed_entry(entry), i)
+                   for i, entry in enumerate(data_to_parse))
 
-  entries = []
-  errors = []
+    entries = []
+    errors = []
 
-  for entry, line in raw_entries:
-    if entry:
-      entries.append(entry)
-    else:
-      errors.append(line)
+    for entry, line in raw_entries:
+        if entry:
+            entries.append(entry)
+        else:
+            errors.append(line)
 
-  entries.sort(key=lambda entry: '%s %s' % (entry['lastname'],
-                                            entry['firstname']))
-  return dict(entries=entries, errors=errors)
+    entries.sort(key=lambda entry: '%s %s' % (entry['lastname'],
+                                              entry['firstname']))
+    return dict(entries=entries, errors=errors)
+
 
 def main():
-  """The main body of the program."""
-  with open('data.in', 'r') as input_file:
-    with open('result.out', 'w') as output_file:
-      parsed_data = parse_data(input_file)
-      json_formatted_data = json.dumps(parsed_data, sort_keys=True, indent=2)
-      output_file.write(json_formatted_data)
-  
+    """The main body of the program."""
+    with open('data.in', 'r') as input_file:
+        with open('result.out', 'w') as output_file:
+            parsed_data = parse_data(input_file)
+            json_formatted_data = json.dumps(
+                parsed_data, sort_keys=True, indent=2)
+            output_file.write(json_formatted_data)
+
+
 if __name__ == '__main__':
-  main()
+    main()
--- tests/percolate_test.py
+++ tests/percolate_test.py
@@ -5,173 +5,175 @@ from solution import percolate
 
 
 class HelperTests(unittest.TestCase):
-  def testParseFormat_GivenInputYieldsExpectedOutput(self):
-    sample_data = [
-      'Booker T., Washington, 87360, 373 781 7380, yellow',
-      'Chandler, Kerri, (623)-668-9293, pink, 123123121',
-      'James Murphy, yellow, 83880, 018 154 6474',
-      'asdfawefawea',
-    ]
+    def testParseFormat_GivenInputYieldsExpectedOutput(self):
+        sample_data = [
+            'Booker T., Washington, 87360, 373 781 7380, yellow',
+            'Chandler, Kerri, (623)-668-9293, pink, 123123121',
+            'James Murphy, yellow, 83880, 018 154 6474',
+            'asdfawefawea',
+        ]
 
-    percolate_expected_output = {
-      "entries": [
-      {
-        "color": "yellow", 
-        "firstname": "James", 
-        "lastname": "Murphy", 
-        "phonenumber": "018-154-6474", 
-        "zipcode": "83880"
-      },
-      {
-        "color": "yellow", 
-        "firstname": "Booker T.", 
-        "lastname": "Washington", 
-        "phonenumber": "373-781-7380", 
-        "zipcode": "87360"
-      }
-      ],
-      "errors": [
-        1,
-        3
-      ]
-    }
+        percolate_expected_output = {
+            "entries": [
+                {
+                    "color": "yellow",
+                    "firstname": "James",
+                    "lastname": "Murphy",
+                    "phonenumber": "018-154-6474",
+                    "zipcode": "83880"
+                },
+                {
+                    "color": "yellow",
+                    "firstname": "Booker T.",
+                    "lastname": "Washington",
+                    "phonenumber": "373-781-7380",
+                    "zipcode": "87360"
+                }
+            ],
+            "errors": [
+                1,
+                3
+            ]
+        }
 
-    result = percolate.parse_data(sample_data)
-    self.assertEquals(result, percolate_expected_output)
+        result = percolate.parse_data(sample_data)
+        self.assertEquals(result, percolate_expected_output)
 
-  def test_strip_nonnumeric_characters(self):
-    input_data = "$*%&^()3432$5 %asj$553309"
-    expected = "34325553309"
-    result = percolate.strip_nonnumeric_characters(input_data)
-    self.assertEquals(expected, result)
- 
+    def test_strip_nonnumeric_characters(self):
+        input_data = "$*%&^()3432$5 %asj$553309"
+        expected = "34325553309"
+        result = percolate.strip_nonnumeric_characters(input_data)
+        self.assertEquals(expected, result)
+
 
 class FormatParserTests(unittest.TestCase):
-  def setUp(self):
-    self.test_data = 'Booker T., Washington, 87360, 373 781 7380, yellow'
+    def setUp(self):
+        self.test_data = 'Booker T., Washington, 87360, 373 781 7380, yellow'
 
-  def testFormatPhoneNumber(self):
-    format_parser = percolate.FormatParser(self.test_data)
+    def testFormatPhoneNumber(self):
+        format_parser = percolate.FormatParser(self.test_data)
 
-    valid_number1 = "373-781-7380"
-    valid_number2 = "(623)-668-9293"
-    valid_number3 = "3737817380"
-    valid_number4 = "454 934 6454"
-    invalid_number = "11111111111"
+        valid_number1 = "373-781-7380"
+        valid_number2 = "(623)-668-9293"
+        valid_number3 = "3737817380"
+        valid_number4 = "454 934 6454"
+        invalid_number = "11111111111"
 
-    self.assertEquals(format_parser.FormatPhoneNumber(valid_number1),
-                      '373-781-7380')
-    self.assertEquals(format_parser.FormatPhoneNumber(valid_number2),
-                      '623-668-9293')
-    self.assertEquals(format_parser.FormatPhoneNumber(valid_number3),
-                      '373-781-7380')
-    self.assertEquals(format_parser.FormatPhoneNumber(valid_number4),
-                      '454-934-6454')
-    self.assertFalse(format_parser.FormatPhoneNumber(invalid_number))
+        self.assertEquals(format_parser.FormatPhoneNumber(valid_number1),
+                          '373-781-7380')
+        self.assertEquals(format_parser.FormatPhoneNumber(valid_number2),
+                          '623-668-9293')
+        self.assertEquals(format_parser.FormatPhoneNumber(valid_number3),
+                          '373-781-7380')
+        self.assertEquals(format_parser.FormatPhoneNumber(valid_number4),
+                          '454-934-6454')
+        self.assertFalse(format_parser.FormatPhoneNumber(invalid_number))
 
-  def testIsValidZipcode(self):
-    format_parser = percolate.FormatParser(self.test_data)
+    def testIsValidZipcode(self):
+        format_parser = percolate.FormatParser(self.test_data)
 
-    valid_zipcode = '10003'
-    invalid_zipcode1 = '12345678'
-    invalid_zipcode2 = 'abcde'
+        valid_zipcode = '10003'
+        invalid_zipcode1 = '12345678'
+        invalid_zipcode2 = 'abcde'
 
-    self.assertTrue(format_parser.IsValidZipcode(valid_zipcode))
-    self.assertFalse(format_parser.IsValidZipcode(invalid_zipcode1))
-    self.assertFalse(format_parser.IsValidZipcode(invalid_zipcode2))
+        self.assertTrue(format_parser.IsValidZipcode(valid_zipcode))
+        self.assertFalse(format_parser.IsValidZipcode(invalid_zipcode1))
+        self.assertFalse(format_parser.IsValidZipcode(invalid_zipcode2))
 
 
 class FormatParser1Tests(unittest.TestCase):
-  def testFormatParser(self):
-    inputted = 'Zlotnik, Zack, 123-456-7890, Blue, 12345'
-    expected = {
-      "color": "Blue",
-      "firstname": "Zack",
-      "lastname": "Zlotnik",
-      "phonenumber": "123-456-7890",
-      "zipcode": "12345"
-    }
+    def testFormatParser(self):
+        inputted = 'Zlotnik, Zack, 123-456-7890, Blue, 12345'
+        expected = {
+            "color": "Blue",
+            "firstname": "Zack",
+            "lastname": "Zlotnik",
+            "phonenumber": "123-456-7890",
+            "zipcode": "12345"
+        }
 
-    format_parser = percolate.FormatParser1(inputted)
-    self.assertEquals(format_parser.GetFormattedData(), expected)
+        format_parser = percolate.FormatParser1(inputted)
+        self.assertEquals(format_parser.GetFormattedData(), expected)
 
-  def testInvalidFormats(self):
-    invalid_data = [
-      'James Murphy, yellow, 83880, 018 154 6474',
-      'Booker T., Washington, 87360, 373 781 7380, yellow',
-      'Chandler, Kerri, (623)-668-9293, pink, 123123121',
-      'asdfawefawea' 
-    ]
+    def testInvalidFormats(self):
+        invalid_data = [
+            'James Murphy, yellow, 83880, 018 154 6474',
+            'Booker T., Washington, 87360, 373 781 7380, yellow',
+            'Chandler, Kerri, (623)-668-9293, pink, 123123121',
+            'asdfawefawea'
+        ]
 
-    for item in invalid_data:
-      parsed_data = percolate.FormatParser1(item).GetFormattedData()
-      self.assertFalse(parsed_data)
+        for item in invalid_data:
+            parsed_data = percolate.FormatParser1(item).GetFormattedData()
+            self.assertFalse(parsed_data)
 
 
 class FormatParser2Tests(unittest.TestCase):
-  def testFormatParser(self):
-    inputted = 'James Murphy, yellow, 83880, 018 154 6474'
-    expected = {
-      "color": "yellow",
-      "firstname": "James",
-      "lastname": "Murphy",
-      "phonenumber": "018-154-6474",
-      "zipcode": "83880"
-    }
+    def testFormatParser(self):
+        inputted = 'James Murphy, yellow, 83880, 018 154 6474'
+        expected = {
+            "color": "yellow",
+            "firstname": "James",
+            "lastname": "Murphy",
+            "phonenumber": "018-154-6474",
+            "zipcode": "83880"
+        }
 
-    format_parser = percolate.FormatParser2(inputted)
-    self.assertEquals(format_parser.GetFormattedData(), expected)
+        format_parser = percolate.FormatParser2(inputted)
+        self.assertEquals(format_parser.GetFormattedData(), expected)
 
-  def testFormatParser_MiddleInitial(self):
-    inputted = 'Englebert G. Humperdink, red, 36410, 839 014 8051'
-    expected = {
-      "color": "red",
-      "firstname": "Englebert G.",
-      "lastname": "Humperdink",
-      "zipcode": "36410",
-      "phonenumber": "839-014-8051"
-    }
+    def testFormatParser_MiddleInitial(self):
+        inputted = 'Englebert G. Humperdink, red, 36410, 839 014 8051'
+        expected = {
+            "color": "red",
+            "firstname": "Englebert G.",
+            "lastname": "Humperdink",
+            "zipcode": "36410",
+            "phonenumber": "839-014-8051"
+        }
 
-    format_parser = percolate.FormatParser2(inputted)
-    self.assertEquals(format_parser.GetFormattedData(), expected)
+        format_parser = percolate.FormatParser2(inputted)
+        self.assertEquals(format_parser.GetFormattedData(), expected)
 
-  def testInvalidFormats(self):
-    invalid_data = [
-      'Zlotnik, Zack, 123-456-7890, Blue, 12345',
-      'Booker T., Washington, 87360, 373 781 7380, yellow'
-      'Chandler, Kerri, (623)-668-9293, pink, 123123121',
-      'asdfawefawea' 
-    ]
+    def testInvalidFormats(self):
+        invalid_data = [
+            'Zlotnik, Zack, 123-456-7890, Blue, 12345',
+            'Booker T., Washington, 87360, 373 781 7380, yellow'
+            'Chandler, Kerri, (623)-668-9293, pink, 123123121',
+            'asdfawefawea'
+        ]
 
-    for item in invalid_data:
-      parsed_data = percolate.FormatParser2(item).GetFormattedData()
-      self.assertFalse(parsed_data)
+        for item in invalid_data:
+            parsed_data = percolate.FormatParser2(item).GetFormattedData()
+            self.assertFalse(parsed_data)
+
 
 class FormatParser3Tests(unittest.TestCase):
-  def testFormatParser(self):
-    inputted = 'Booker T., Washington, 87360, 373 781 7380, yellow' 
-    expected = {
-      "color": "yellow",
-      "firstname": "Booker T.",
-      "lastname": "Washington",
-      "phonenumber": "373-781-7380",
-      "zipcode": "87360"
-    }
+    def testFormatParser(self):
+        inputted = 'Booker T., Washington, 87360, 373 781 7380, yellow'
+        expected = {
+            "color": "yellow",
+            "firstname": "Booker T.",
+            "lastname": "Washington",
+            "phonenumber": "373-781-7380",
+            "zipcode": "87360"
+        }
 
-    format_parser = percolate.FormatParser3(inputted)
-    self.assertEquals(format_parser.GetFormattedData(), expected)
+        format_parser = percolate.FormatParser3(inputted)
+        self.assertEquals(format_parser.GetFormattedData(), expected)
 
-  def testInvalidFormats(self):
-    invalid_data = [
-      'James Murphy, yellow, 83880, 018 154 6474',
-      'Zlotnik, Zack, 123-456-7890, Blue, 12345',
-      'Chandler, Kerri, (623)-668-9293, pink, 123123121',
-      'asdfawefawea' 
-    ]
+    def testInvalidFormats(self):
+        invalid_data = [
+            'James Murphy, yellow, 83880, 018 154 6474',
+            'Zlotnik, Zack, 123-456-7890, Blue, 12345',
+            'Chandler, Kerri, (623)-668-9293, pink, 123123121',
+            'asdfawefawea'
+        ]
 
-    for item in invalid_data:
-      parsed_data = percolate.FormatParser3(item).GetFormattedData()
-      self.assertFalse(parsed_data)
+        for item in invalid_data:
+            parsed_data = percolate.FormatParser3(item).GetFormattedData()
+            self.assertFalse(parsed_data)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
